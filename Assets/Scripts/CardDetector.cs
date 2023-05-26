8ed307239c072a0dd5a035ba543a319649ae3f32
AnnNeveu <anouchka.neveu@gmail.com>
2023-05-27 00:20:04 +0200

    feat: win condition

--- Assets/Scripts/CardDetector.cs
+++ Assets/Scripts/CardDetector.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class CardDetector : MonoBehaviour
 {
 
     public int CardNumber;
+    [SerializeField] WinCondition winCondition;
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == $"card{CardNumber}") {
             // User used the correct card
+            winCondition.RightCount++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == $"card{CardNumber}") {
+            // User used the correct card
+            winCondition.RightCount--;
         }
     }
 }
