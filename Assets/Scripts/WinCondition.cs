4fc5b552ee0c9bce485c4cb003e0e8b2efbfb11f
AnnNeveu <anouchka.neveu@gmail.com>
2023-05-27 00:41:02 +0200

    fix: push all files

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+
+    public int RightCount;
+    [SerializeField] ParticleSystem winParticles;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void WinScreen() {
+        if (RightCount == 4) {
+            // Win
+            Debug.Log("You win!");
+            winParticles.Play();
+        }
+    }
+
+}
